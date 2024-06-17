96baf92c3c4c0ca4759ddcfb5009fd1975482410
re-hiramatsu <re-hiramatsu>
2024-06-17 11:54:05 +0900

    Product.cs実装

---
+++ Sales.Core/Consts.cs
@@ -0,0 +1,8 @@
+﻿namespace Sales.Core
+{
+    public class Consts
+    {
+        public static readonly string INVENTORY_QUANTITY_ERROR_MESSAGE = "在庫数を上回っています。もう一度入力してください。";
+        public static readonly string INPUT_ERROR_MESSAGE = "不正な値が入力されました。";
+    }
+}
---
+++ Sales.Core/Product.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Sales.Core
+{
+    public class Product
+    {
+        public string ProductName { get; }
+        public int PurchasePrice { get; }
+        public DateTime PurchaseDate { get; }
+        public DateTime SalesDate { get; }
+        public int SalesPrice { get; }
+        public int SalesQuantity { get; private set; }
+        public int StockQuantity { get; private set; }
+
+        public Product(string productName, int purchaseQuantity, int purchasePrice, int salesPrice)
+        {
+            PurchaseDate = DateTime.Now;
+            ProductName = productName;
+
+            if (purchaseQuantity > 0 && purchasePrice > 0 && salesPrice > 0)
+            {
+                StockQuantity = purchaseQuantity;
+                PurchasePrice = purchasePrice;
+                SalesPrice = salesPrice;
+            }
+            else
+            {
+                throw new Exception(Consts.INPUT_ERROR_MESSAGE);
+            }
+        }
+
+        public int GetSalesAmount()
+        {
+            return SalesPrice * SalesQuantity;
+        }
+
+        public int GetIncomeAmount()
+        {
+            return (SalesPrice - PurchasePrice) * SalesQuantity;
+        }
+
+        public void SetStockQuantity(int quantity)
+        {
+            if (quantity > 0 && StockQuantity >= quantity)
+            {
+                SalesQuantity = quantity;
+                StockQuantity -= quantity;
+            }
+            else
+            {
+                throw new Exception(Consts.INPUT_ERROR_MESSAGE);
+            }
+        }
+    }
+}
