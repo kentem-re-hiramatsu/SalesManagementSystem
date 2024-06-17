33c615ff78acb19cf5d4249c0c275836c2b89c9a
re-hiramatsu <re-hiramatsu>
2024-06-17 13:14:29 +0900

    Manager実装

---
+++ Sales.Core/Manager/ProductManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Sales.Core.Manager
+{
+    public class ProductManager
+    {
+        private List<Product> _salesList = new List<Product>();
+
+        public IReadOnlyCollection<Product> SalesList { get { return _salesList; } }
+
+        public void Add(Product product)
+        {
+            _salesList.Add(product);
+        }
+
+        public Product GetSalesList(int index)
+        {
+            return _salesList[index];
+        }
+
+        public int GetTotalSalesAmount()
+        {
+            return 0;
+        }
+
+        public int GetTotalIncomeAmount()
+        {
+            return 0;
+        }
+    }
+}
