8737af274fa78fa2328e73e6b5274f3b95f0ac96
re-hiramatsu <re-hiramatsu>
2024-06-18 11:57:40 +0900

    SalesManager.cs実装

---
+++ Sales.Core/Manager/SalesManager.cs
@@ -0,0 +1,46 @@
+﻿using Product.Core.Model;
+using System.Collections.Generic;
+
+namespace Product.Core.Manager
+{
+    public class SalesManager
+    {
+        private List<Sales> _salesList = new List<Sales>();
+
+        public IReadOnlyCollection<Sales> SalesList { get { return _salesList; } }
+
+        public void Add(Sales sales)
+        {
+            _salesList.Add(sales);
+        }
+
+        public Sales GetSales(int index)
+        {
+            return _salesList[index];
+        }
+
+        public int GetTotalSalesAmount()
+        {
+            var totalSalesAmount = 0;
+
+            foreach (Sales sales in _salesList)
+            {
+                totalSalesAmount += sales.GetSalesAmount();
+            }
+
+            return totalSalesAmount;
+        }
+
+        public int GetTotalIncomeAmount()
+        {
+            var totalIncomeAmount = 0;
+
+            foreach (Sales sales in _salesList)
+            {
+                totalIncomeAmount += sales.GetIncomeAmount();
+            }
+
+            return totalIncomeAmount;
+        }
+    }
+}
