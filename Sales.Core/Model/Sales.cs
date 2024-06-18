80ef1c25731636fac4d7ba83af7d89a16a8a758f
re-hiramatsu <re-hiramatsu>
2024-06-18 11:23:55 +0900

    Sales.csのコンストラクタの引数をPurchaseに変更のためテストケース修正とSales.csの実装

--- Sales.Core/Model/Sales.cs
+++ Sales.Core/Model/Sales.cs
@@ -5,22 +5,47 @@ namespace Product.Core.Model
     public class Sales
     {
         public int SalesPrice { get; }
-        public int SalesQuantity { get; }
+        public int SalesQuantity { get; private set; }
         public DateTime SalesDataTime { get; }
-        public int StockQuantity { get; }
+        public int StockQuantity { get; private set; }
+        private Purchase _purchase;
 
-        public Sales(int price, int purchaseQuantity)
+        public Sales(int price, Purchase purchase)
         {
-            if (price > 0 && purchaseQuantity > 0)
+            if (price > 0)
             {
                 SalesPrice = price;
-                StockQuantity = purchaseQuantity;
             }
             else
             {
                 throw new Exception(Consts.INPUT_ERROR_MESSAGE);
             }
+            StockQuantity = purchase.PurchaseQuantity;
+            _purchase = purchase;
             SalesDataTime = DateTime.Now;
+        }
+
+        public int GetSalesAmount()
+        {
+            return SalesPrice * SalesQuantity;
+        }
+
+        public int GetIncomeAmount()
+        {
+            return GetSalesAmount() - _purchase.PurchasePrice * SalesQuantity;
+        }
+
+        public void ProcessSales(int salesQuantity)
+        {
+            if (salesQuantity > 0 && StockQuantity >= salesQuantity)
+            {
+                SalesQuantity += salesQuantity;
+                StockQuantity -= salesQuantity;
+            }
+            else
+            {
+                throw new Exception(Consts.INPUT_ERROR_MESSAGE);
+            }
         }
     }
 }
--- SalesManagementTest/ModelTest/SalesTest.cs
+++ SalesManagementTest/ModelTest/SalesTest.cs
@@ -7,41 +7,40 @@ namespace SalesManagementTest.ModelTest
     [TestClass]
     public class SalesTest
     {
+        Purchase purchase = new Purchase("ばなな", 8, 100);
+
         [TestMethod]
         public void SalesConstructorTest()
         {
-            var sales = new Sales(100, 5);
+            var sales = new Sales(100, purchase);
             var nowDataTime = DateTime.Now;
 
             Assert.AreEqual(100, sales.SalesPrice);
-            Assert.AreEqual(5, sales.StockQuantity);
+            Assert.AreEqual(8, sales.StockQuantity);
             Assert.AreEqual(nowDataTime, sales.SalesDataTime);
 
-            Assert.ThrowsException<Exception>(() => new Sales(0, 5));
-            Assert.ThrowsException<Exception>(() => new Sales(-1, 5));
-
-            Assert.ThrowsException<Exception>(() => new Sales(100, 0));
-            Assert.ThrowsException<Exception>(() => new Sales(100, -1));
+            Assert.ThrowsException<Exception>(() => new Sales(0, purchase));
+            Assert.ThrowsException<Exception>(() => new Sales(-1, purchase));
         }
 
         [TestMethod]
         public void ProcessSalesTest()
         {
-            var sales = new Sales(100, 5);
-            Assert.AreEqual(5, sales.StockQuantity);
+            var sales = new Sales(100, purchase);
+            Assert.AreEqual(8, sales.StockQuantity);
 
             sales.ProcessSales(3);
             Assert.AreEqual(3, sales.SalesQuantity);
-            Assert.AreEqual(2, sales.StockQuantity);
+            Assert.AreEqual(5, sales.StockQuantity);
         }
 
         [TestMethod]
         public void OverProcessSalesTest()
         {
-            var sales = new Sales(100, 5);
-            Assert.AreEqual(5, sales.StockQuantity);
+            var sales = new Sales(100, purchase);
+            Assert.AreEqual(8, sales.StockQuantity);
 
-            Assert.ThrowsException<Exception>(() => sales.ProcessSales(6));
+            Assert.ThrowsException<Exception>(() => sales.ProcessSales(9));
             Assert.ThrowsException<Exception>(() => sales.ProcessSales(0));
             Assert.ThrowsException<Exception>(() => sales.ProcessSales(-1));
         }
@@ -49,7 +48,7 @@ namespace SalesManagementTest.ModelTest
         [TestMethod]
         public void TotaltSalesAmountAndTotalIncomeAmountTest()
         {
-            var sales = new Sales(200, 4);
+            var sales = new Sales(200, purchase);
             sales.ProcessSales(3);
 
             var salesPrice = 200;
@@ -57,8 +56,6 @@ namespace SalesManagementTest.ModelTest
             var salesAmount = salesPrice * salesQuantity;
 
             Assert.AreEqual(salesAmount, sales.GetSalesAmount());
-
-            var purchase = new Purchase("バナナ", 6, 50);
 
             var purchasePrice = purchase.PurchasePrice;
             var costPrice = purchasePrice * salesQuantity;
