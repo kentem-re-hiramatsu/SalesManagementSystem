54c3bcdd53b3cd41b966200d2457400a42431a2e
re-hiramatsu <re-hiramatsu>
2024-06-18 10:02:55 +0900

    Purchase.cs実装

---
+++ Sales.Core/Model/Purchase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Sales.Core.Model
+{
+    public class Purchase
+    {
+        public string ProductName { get; }
+        public int PurchaseQuantity { get; }
+        public int PurchasePrice { get; }
+        public DateTime PurchaseDateTime { get; }
+
+        public Purchase(string name, int purchaseQuantity, int price)
+        {
+            ProductName = name;
+
+            if (purchaseQuantity > 0 && price > 0)
+            {
+                PurchaseQuantity = purchaseQuantity;
+                PurchasePrice = price;
+            }
+            else
+            {
+                throw new Exception(Consts.INPUT_ERROR_MESSAGE);
+            }
+            PurchaseDateTime = DateTime.Now;
+        }
+    }
+}
